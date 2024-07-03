078b5bd83acba4af6b9adeee1c7dc87324b33473
Kayla Tran <kaytran@pdx.edu>
2024-07-03 12:51:27 -0700

    lab 1 done with name, hometown and major

---
+++ Lab1/hello.py
@@ -0,0 +1,15 @@
+"""
+this is supposed to display name and hometown
+"""
+
+def main():
+    name = "Kayla Tran"
+    hometown = "Portland, Oregon"
+    major = "Computer Science"
+
+    print(name)
+    print(hometown)
+    print(major)
+
+if __name__ == "__main__":
+    main()
